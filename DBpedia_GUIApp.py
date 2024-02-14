6e05f0b99c358788d94341c280132c1df5ad61f5
kyuy <naganegi.1st@gmail.com>
2024-02-14 22:37:04 +0900

    laptopのモニタでも見れるようサイズを調整

--- DBpedia_GUIApp.py
+++ DBpedia_GUIApp.py
@@ -20,7 +20,7 @@ def unique_value_generator(start_value=0):
 def main():
   ImgKey = "img"
   InduceSearchTextKey = "induce"
-  SearchWordKey = "search_word"
+  SearchBoxKey = "search_word"
   DoSearchKey = "do_search"
   AbstTextKey = "abst"
   ErrorTextKey = "error"
@@ -36,9 +36,9 @@ def main():
 
   layout = [
     [
-      sg.Frame("graph", [[sg.Image("white.png", key=ImgKey)]]),
+      sg.Frame("graph", [[sg.Image("white.png", key=ImgKey, size=(700, 500))]]),
       sg.Frame("operate", [
-        [sg.Text("キーワードを入力してください：", key=InduceSearchTextKey), sg.InputText(size=(20,1), key=SearchWordKey), sg.Button("検索", key=DoSearchKey)],
+        [sg.Text("キーワードを入力してください：", key=InduceSearchTextKey), sg.InputText(size=(20,1), key=SearchBoxKey), sg.Button("検索", key=DoSearchKey)],
         [sg.Text("概要：")],
         [sg.Text("", key=AbstTextKey)],
         [sg.Text("", key=ErrorTextKey)],
@@ -48,7 +48,7 @@ def main():
     ],
   ]
 
-  window = sg.Window("深化型学習支援システム", layout, resizable=True)
+  window = sg.Window("深化型学習支援システム", layout, resizable=True, size=(1200,600))
 
   # event loop
   while True:
@@ -57,7 +57,8 @@ def main():
       break
     if event == DoSearchKey:
       if search_mode == "keyword":
-        keyword = values[SearchWordKey]
+        keyword = values[SearchBoxKey]
+        window[SearchBoxKey].update("")
         result_entities = do_inquiry(quereis.create_query_for_get_object_from_keyword(keyword))
         result_entities = reshapeResults.reshape_for_object(result_entities)
         if not result_entities:
