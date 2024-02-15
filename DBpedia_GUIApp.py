2c2409c5b9d0cfb61b8e3fabf28368400a43d348
kyuy <naganegi.1st@gmail.com>
2024-02-15 19:38:28 +0900

    リスト選択がリテラルの場合とリストモードがプロパティの場合の機能追加

--- DBpedia_GUIApp.py
+++ DBpedia_GUIApp.py
@@ -4,6 +4,7 @@ from quereis import do_inquiry
 import reshapeResults
 import myfunc
 import manipulate_tree
+import manipulate_graph
 try:
   from typing import Literal
 except ImportError:
@@ -22,6 +23,7 @@ def main():
   InduceSearchTextKey = "induce"
   SearchBoxKey = "search_word"
   DoSearchKey = "do_search"
+  AbstTitleKey = "abst_title"
   AbstTextKey = "abst"
   ErrorTextKey = "error"
   ChoiceFromListKey = "choise_from_list"
@@ -40,7 +42,7 @@ def main():
       sg.Frame("graph", [[sg.Image("white.png", key=ImgKey, size=(700, 500))]]),
       sg.Frame("operate", [
         [sg.Text("キーワードを入力してください：", key=InduceSearchTextKey), sg.InputText(size=(20,1), key=SearchBoxKey), sg.Button("検索", key=DoSearchKey)],
-        [sg.Text("概要：")],
+        [sg.Text("概要：", key=AbstTitleKey)],
         [sg.Text("", key=AbstTextKey)],
         [sg.Text("", key=ErrorTextKey)],
         [sg.Text("検索結果", key=ListBoxTitle), sg.Button("決定", key=ChoiceFromListKey)],
@@ -70,8 +72,8 @@ def main():
         list_mode = "entity"
         search_mode = "ID"
     if event == ChoiceFromListKey:
+      choise_ID = myfunc.extract_first_number(values[ListBoxKey][0])
       if list_mode == "entity":
-        choise_ID = myfunc.extract_first_number(values[ListBoxKey][0])
         window[ListBoxKey].update([])
         if result_entities[choise_ID]["type"] == "entity":
           result_property = do_inquiry(quereis.create_query_for_get_property_from_object(objectURI=result_entities[choise_ID]["URI"]))
@@ -79,6 +81,7 @@ def main():
           if not result_property:
             window[ErrorTextKey].update("そのエンティティはプロパティを持っていません", text_color="#FFFFFF", background_color="#FF0000")
             continue
+          window[ErrorTextKey].update("", background_color="#64778D")
           node_ID = next(ID_generator)
           nodes_dict[node_ID] = manipulate_tree.create_node(
             name=result_entities[choise_ID]["label"],
@@ -94,7 +97,46 @@ def main():
           window[ListBoxKey].update(["["+str(i)+"]"+str(result_property[i]["label"]) for i in range(len(result_property))])
           list_mode = "property"
         elif result_entities[choise_ID]["type"] == "literal":
-          pass
+          node_ID = next(ID_generator)
+          nodes_dict[node_ID] = manipulate_tree.create_node(
+            name=result_entities[choise_ID]["value"],
+            node_ID=node_ID,
+            URI=None,
+            node_type="literal",
+            parent_node=current_parent_node
+          )
+          window[AbstTitleKey].update("リテラル値")
+          window[AbstTextKey].update(nodes_dict[node_ID].name)
+          window[ListBoxKey].update([])
+          list_mode = None
+        if node_ID <= 1:
+            continue
+        manipulate_graph.renew_graph_pic(nodes_dict[start_ID])
+        window[ImgKey].update("graph.png")
+      elif list_mode == "property":
+        result_entities = do_inquiry(
+          quereis.create_query_for_get_object_from_set_of_entity_and_property(
+            objectURI=current_parent_node.URI, propertyURI=result_property[choise_ID]["URI"]
+          )
+        )
+        result_entities = reshapeResults.reshape_for_object(result_entities)
+        if not result_entities:
+          window[ErrorTextKey].update("そのプロパティはエンティティを持っていません.",text_color="#FFFFFF", background_color="#FF0000")
+          continue
+        window[ErrorTextKey].update("", background_color="#64778D")
+        node_ID = next(ID_generator)
+        nodes_dict[node_ID] = manipulate_tree.create_node(
+          name=result_property[choise_ID]["label"],
+          node_ID=node_ID,
+          URI=result_property[choise_ID]["URI"],
+          node_type="property",
+          parent_node=current_parent_node
+        )
+        current_parent_node = nodes_dict[node_ID]
+        window[ListBoxTitle].update("エンティティ一覧")
+        window[ListBoxKey].update(["["+str(i)+"]"+str(result_entities[i]["label"]) for i in range(len(result_entities))])
+        list_mode = "entity"
+
 
   window.close()
 
